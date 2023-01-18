58e5ae4da5029dc65776c0b289a9b1d51cbbb818
SamuelYudiG <samuelyudigun@gmail.com>
2023-01-18 08:12:56 +0700

    fix: [SY] Import Log Class

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
