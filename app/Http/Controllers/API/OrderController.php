8ced328310997d0ff0cf43b0c3116d8015b877be
SamuelYudiG <samuelyudigun@gmail.com>
2023-03-01 09:59:36 +0700

    fix: [SY] Change signature key rules

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -101,7 +101,7 @@ class OrderController extends Controller
             $order = Order::findOrFail($data->order_id);
 
             // Construct the signature key using the order details and your merchant server key
-            $signatureKey = md5($order->id . $order->status_code . $order->gross_amount . Config::$serverKey);
+            $signatureKey = md5($data->order_id . $data->status_code . $data->gross_amount . Config::$serverKey);
 
             // Verify the signature key
             if ($signatureKey !== $data->signature_key) {
